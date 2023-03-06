00ea9aa398d979d14b35e9de1a330c4886b58ec1
baku2426@gmail.com <baku2426@gmail.com>
2023-03-06 10:11:42 +0100

    Save for macos

--- src/check_access.c
+++ src/check_access.c
@@ -6,11 +6,27 @@
 /*   By: my_name_ <my_name_@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 00:19:30 by my_name_          #+#    #+#             */
-/*   Updated: 2023/03/06 01:29:03 by my_name_         ###   ########.fr       */
+/*   Updated: 2023/03/06 10:10:08 by my_name_         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "src.h"
+
+int	is_tmp_file(char *path)
+{
+	int	length;
+	int	i;
+
+	length = ft_strlen(path);
+	i = length;
+	while (i >= 0)
+	{
+		if (path[i] == '/')
+			break ;
+		i--;
+	}
+	return (ft_strncmp(path, TMP_PATH, i));
+}
 
 int	check_access(char *path)
 {
@@ -22,6 +38,8 @@ int	check_access(char *path)
 		return (IS_DIR);
 	else if (S_ISREG(inf.st_mode))
 		return (IS_FILE);
+	else if (access(path, F_OK) && is_tmp_file(path))
+		return (NOT_EXIST);
 	return (IS_NOTHING);
 }
 
