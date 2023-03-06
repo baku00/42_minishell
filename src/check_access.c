3b6c05df371e307fe08c705c10a4c77d0bde44a9
baku2426@gmail.com <baku2426@gmail.com>
2023-03-06 01:29:11 +0100

    Save for macos

--- src/check_access.c
+++ src/check_access.c
@@ -6,7 +6,7 @@
 /*   By: my_name_ <my_name_@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 00:19:30 by my_name_          #+#    #+#             */
-/*   Updated: 2023/03/06 00:54:33 by my_name_         ###   ########.fr       */
+/*   Updated: 2023/03/06 01:29:03 by my_name_         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,18 @@ int	check_access(char *path)
 		return (IS_FILE);
 	return (IS_NOTHING);
 }
+
+// int	check_access(char *path)
+// {
+// 	struct stat	inf;
+
+// 	inf.st_mode = 0;
+// 	lstat(path, &inf);
+// 	if (S_ISDIR(inf.st_mode))
+// 		return (IS_DIR);
+// 	else if (S_ISREG(inf.st_mode))
+// 		return (IS_FILE);
+// 	else if (access(path, F_OK))
+// 		return (NOT_EXIST);
+// 	return (IS_NOTHING);
+// }
